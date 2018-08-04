f7c8af7d1a607638020c92008606cf8f6216a20e
Shuhei Kagaya <shuhei.lab@gmail.com>
2018-08-04 21:35:28 +0900

    ユーザー登録のindexメソッド作成
    * ユーザー登録のindexメソッド作成

--- app/Http/Controllers/SignupController.php
+++ app/Http/Controllers/SignupController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class SignupController extends Controller
 {
-    //
+    protected $sessionKey = 'SignupData';
+
+    public function index(User $user)
+    {
+        if ($data = \Session::get($this->sessionKey)) {
+            $user->fill($data);
+        }
+
+        return view('signup.index')->with(compact('user'));
+    }
 }
