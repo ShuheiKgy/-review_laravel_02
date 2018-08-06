2b5c570bfdcb1626092046b2654bd29065f138a2
Shuhei Kagaya <shuhei.lab@gmail.com>
2018-08-06 12:11:05 +0900

    管理画面用Route作成
    * 管理画面用Route作成

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,18 @@ Route::get('signup/confirm', 'SignupController@confirm')->name('signup.confirm')
 Route::post('signup/confirm', 'SignupController@postConfirm');
 
 Route::get('signup/thanks', 'SignupController@thanks')->name('signup.thanks');
+
+// 管理画面
+Route::prefix('admin')->namespace('Admin')->as('admin.')->group(function () {
+    Route::middleware('guest:admin')->group(function () {
+        Route::get('login', 'LoginController@showLoginForm')->name('login');
+
+        Route::post('login', 'LoginController@login');
+    });
+
+    Route::middleware('auth:admin')->group(function () {
+        Route::get('logout', 'LoginController@logout')->name('logout');
+
+        Route::get('', 'LoginController@index')->name('top');
+    });
+});
