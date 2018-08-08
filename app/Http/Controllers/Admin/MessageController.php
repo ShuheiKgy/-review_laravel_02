0ce21639e40356f85c543793c7ec6167a99496fe
Shuhei Kagaya <shuhei.lab@gmail.com>
2018-08-08 21:24:08 +0900

    indexメソッド実装
    * indexメソッド実装

--- app/Http/Controllers/Admin/MessageController.php
+++ app/Http/Controllers/Admin/MessageController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Message;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class MessageController extends Controller
 {
-    //
+    public function index()
+    {
+        $messages = Message::latest()->with('user')->get();
+
+        return view('admin.message.index')->with(compact('messages'));
+    }
 }
