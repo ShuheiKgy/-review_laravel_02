06eba7ac2fd1aed388cc8ac0a47c7b029e5b824f
Shuhei Kagaya <shuhei.lab@gmail.com>
2018-08-08 21:13:49 +0900

    Messageのルーティング追加
    * Messageのルーティング追加

--- routes/web.php
+++ routes/web.php
@@ -35,5 +35,15 @@ Route::prefix('admin')->namespace('Admin')->as('admin.')->group(function () {
         Route::get('logout', 'LoginController@logout')->name('logout');
 
         Route::get('', 'IndexController@index')->name('top');
+
+        Route::get('message', 'MessageController@index')->name('message.index');
+
+        Route::get('message/create', 'MessageController@create')->name('message.create');
+
+        Route::post('message/create', 'MessageController@store');
+
+        Route::get('message/edit/{message}', 'MessageController@edit')->name('message.edit');
+
+        Route::post('message/edit/{message}', 'MessageController@update');
     });
 });
