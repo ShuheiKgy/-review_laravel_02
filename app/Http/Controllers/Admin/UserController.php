9c5ef351827ad7b87ba01fb5ac88f048b7f63cca
Shuhei Kagaya <shuhei.lab@gmail.com>
2018-08-09 22:39:21 +0900

    UserControllerにindexメソッド追加
    * UserControllerにindexメソッド追加

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+    public function index(Request $request)
+    {
+        $users = User::latest()->get();
+
+        return view('admin.user.index')->with(compact('users'));
+    }
+}
