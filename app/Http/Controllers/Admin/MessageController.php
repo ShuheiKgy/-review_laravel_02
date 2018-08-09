4970202298fb48e13ef7a018ae312f1a4c4de610
Shuhei Kagaya <shuhei.lab@gmail.com>
2018-08-09 17:17:37 +0900

    createメソッド追加
    * createメソッド追加

--- app/Http/Controllers/Admin/MessageController.php
+++ app/Http/Controllers/Admin/MessageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Message;
+use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -13,5 +14,12 @@ class MessageController extends Controller
         $messages = Message::latest()->with('user')->get();
 
         return view('admin.message.index')->with(compact('messages'));
+    }
+
+    public function create(Message $message)
+    {
+        $userlist = User::getUserList();
+
+        return view('admin.message.create')->with(compact('message', 'userlist'));
     }
 }
