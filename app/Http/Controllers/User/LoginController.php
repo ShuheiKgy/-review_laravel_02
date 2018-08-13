caa984ed99c5928b317db2a64d2d53bae5b35a5a
Shuhei Kagaya <shuhei.lab@gmail.com>
2018-08-13 11:58:34 +0900

    User/LoginControllerを実装
    * User/LoginControllerを実装

--- app/Http/Controllers/User/LoginController.php
+++ app/Http/Controllers/User/LoginController.php
@@ -7,5 +7,44 @@ use App\Http\Controllers\Controller;
 
 class LoginController extends Controller
 {
-    //
+    public function redirectTo()
+    {
+        return route('user.top');
+    }
+
+    public function  showLoginForm()
+    {
+        return view('user.login');
+    }
+
+    protected function validateLogin(Request $request)
+    {
+        $messages = [
+            $this->username().'.required' => 'メールアドレスを入力してください。',
+            'password.required' => 'パスワードを入力してください。',
+        ];
+
+        $this->validate($request, [
+            $this->username() => 'required|string',
+            'password' => 'required|string',
+        ], $messages);
+    }
+
+    public function logout(Request $request)
+    {
+        $partialLogin = auth('user')->guest() || auth('admin')->guest();
+
+        $this->guard()->logout();
+
+        if ($partialLogin) {
+            $request->session()->invalidate();
+        }
+
+        return redirect()->route('user.login');
+    }
+
+    protected function guard()
+    {
+        return auth('user');
+    }
 }
