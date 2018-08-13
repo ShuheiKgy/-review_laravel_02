44fab3314943b213684a87e5cd77a30701acf2d8
Shuhei Kagaya <shuhei.lab@gmail.com>
2018-08-13 12:06:14 +0900

    user/login.blade.phpの実装
    * user/login.blade.phpの実装

---
+++ resources/views/admin/user/login.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>ユーザーログイン</title>
+    <link type="text/css" rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+
+<h2>ユーザーログイン</h2>
+
+@if($erros->any())
+    <ul class="error-box">
+        @foreach($errors->all() as $_error)
+            <li>{{ $_error }}</li>
+        @endforeach
+    </ul>
+@endif
+
+<form method="post">
+    {{ csrf_field() }}
+    <ul>
+        <li>
+            <label>メールアドレス</label>
+            <input type="text" name="email" value="{{ old('email') }}">
+        </li>
+        <li>
+            <label>パスワード</label>
+            <input type="password" name="password">
+        </li>
+    </ul>
+
+    <input type="submit" value="ログイン">
+</form>
+</body>
+</html>
