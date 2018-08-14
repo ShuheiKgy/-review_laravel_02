10e6330611bec206d8b25d41ac75940085f22d24
Shuhei Kagaya <shuhei.lab@gmail.com>
2018-08-14 11:17:56 +0900

    user/profile/edit.blade.phpを追加
    * user/profile/edit.blade.phpを追加

---
+++ resources/views/user/profile/edit.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.user')
+
+@section('content')
+
+    <h1>登録者情報変更</h1>
+
+    @if($errors->any())
+        <ul class="error-box">
+            @foreach($errors->all() as $_error)
+                <li>{{ $_error }}</li>
+            @endforeach
+        </ul>
+    @endif
+
+    @if($_flash_msg = $Session::get('$_flash_msg'))
+        <p class="info-box">{{ $_flash_msg }}</p>
+    @endif
+
+    <form method="post">
+        {{ csrf_field() }}
+
+        <ul>
+            <li>
+                <label>名前</label>
+                <input type="text" name="name" value="{{ old('name', $user->name ) }}">
+            </li>
+            <li>
+                <label>メールアドレス</label>
+                <input type="text" name="email" value="{{ old('email', $user->email) }}">
+            </li>
+        </ul>
+
+        <input type="submit" value="更新する">
+    </form>
+@endsection
