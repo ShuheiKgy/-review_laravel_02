3efbcd76d54edf8c066b399d680338dcc5ea0f14
Shuhei Kagaya <shuhei.lab@gmail.com>
2018-08-14 11:09:20 +0900

    editとupdateメソッド実装
    * editとupdateメソッド実装

--- app/Http/Controllers/User/ProfileController.php
+++ app/Http/Controllers/User/ProfileController.php
@@ -7,5 +7,22 @@ use App\Http\Controllers\Controller;
 
 class ProfileController extends Controller
 {
-    //
+    public function edit()
+    {
+        $user = auth()->user();
+
+        return view('user.profile.edit')->with(compact('user'));
+    }
+
+    public function update(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|max:255|email|unique:users,email,' . auth()->id(),
+        ]);
+
+        auth()->user()->update($data);
+
+        return redirect(route('user.profile.edit'))->with('_flash_msg', '変更が完了しました');
+    }
 }
