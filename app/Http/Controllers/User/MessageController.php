cc3c3f7e4b3b6ded05bb961856f8b69789b5ff5a
Shuhei Kagaya <shuhei.lab@gmail.com>
2018-08-14 11:49:43 +0900

    user/message/index.blade.phpを追加
    user/message/index.blade.phpを追加

--- app/Http/Controllers/User/MessageController.php
+++ app/Http/Controllers/User/MessageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\User;
 
+use App\Message;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -9,8 +10,8 @@ class MessageController extends Controller
 {
     public function index(Request $request)
     {
-        $messages = auth()->user()->messages()->latest->get();
+        $messages = auth()->user()->messages()->latest()->get();
 
-        return view(user.message.index)->with(compact('messages'));
+        return view('user.message.index')->with(compact('messages'));
     }
 }
